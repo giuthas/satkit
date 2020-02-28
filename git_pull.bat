5b68675c8dc055417e7440cbec7580ddd24c9535
SRM <Scott.Moisik@ntu.edu.sg>
2020-02-28 15:31:57 +0800

    Update: [2020-02-28 15:31:56.910]

---
+++ git_pull.bat
@@ -0,0 +1,4 @@
+call git status
+call git reset .
+call git status
+call git pull
---
+++ git_push.bat
@@ -0,0 +1,10 @@
+call git status
+call git reset .
+call git status
+call git add *
+@echo off
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
+call git commit -m "Update: [%ldt%]"
+call git push origin master
+PAUSE

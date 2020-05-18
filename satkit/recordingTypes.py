7e0b9e1c807f3e29642b28cd5497a3d15d8d3cf3
Pertti Palo <pertti.palo@taurlin.org>
2020-05-18 10:07:06 +0300

    A bit of progress on implementing the new recording objects.

--- satkit/recordingTypes.py
+++ satkit/recordingTypes.py
@@ -5,16 +5,58 @@ class Recording:
     """
 
     def __init__(self):
-        self.data = []
-
+        self.data = {}
+        self.meta = {}
+        
 
 class Ultrasound_Recording(Recording):
     """
     Superclass for ultrasound recording classes.
     """
 
+    def __init__(self):
+        super.__init__()
+    
 
 class AAA_Ult_Recording(Ultrasound_Recording):
     """
-    Ultrasound recording exported by AAA.
+    Ultrasound recording exported from AAA.
     """
+
+    def __init__(self, filename = None):
+        super.__init__()
+        self.parse_promptfile(filename)
+        
+
+    def parse_promptfile(self, filename):
+    """
+    Read an AAA .txt (not US.txt) file and save prompt, recording date and time,  
+    and participant name into the meta dictionary.
+    """
+    with closing(open(filename, 'r')) as promptfile:
+        lines = promptfile.read().splitlines()
+        self.meta['prompt'] = lines[0]
+        self.meta['date'] = lines[1]
+        # could also do datetime as below, but there doesn't seem to be any reason to so.
+        # date = datetime.strptime(lines[1], '%d/%m/%Y %H:%M:%S')
+        self.meta['participant'] = lines[2].split(',')[0]
+
+        _AAA_logger.debug("Read prompt file " + filename + ".")
+        
+
+    def parse_ult_meta(self, filename):
+    """
+    Parse metadata from an AAA 'US.txt' file into the meta dictionary.
+    """
+    with closing(open(filename, 'r')) as metafile:
+        for line in metafile:
+            (key, value_str) = line.split("=")
+            try:
+                value = int(value_str)
+            except ValueError:
+                value = float(value_str)
+            self.meta[key] = value
+
+        _AAA_logger.debug("Read and parsed ultrasound metafile " + filename + ".")
+
+

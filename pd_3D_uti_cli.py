cca782bb9e48bcb3a0b80ae931a89432e34e5367
Pertti Palo <pertti.palo@taurlin.org>
2021-09-03 00:11:21 +0300

    Script file for running PD on 3D4D ultrasound. No back end yet.

---
+++ pd_3D_uti_cli.py
@@ -0,0 +1,55 @@
+#
+# Copyright (c) 2019-2021 Pertti Palo, Scott Moisik, and Matthew Faytak.
+#
+# This file is part of Speech Articulation ToolKIT
+# (see https://github.com/giuthas/satkit/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# The example data packaged with this program is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License. You should have received a
+# copy of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License along with the data. If not,
+# see <https://creativecommons.org/licenses/by-nc-sa/4.0/> for details.
+#
+# When using the toolkit for scientific publications, please cite the
+# articles listed in README.markdown. They can also be found in
+# citations.bib in BibTeX format.
+#
+
+import time
+import logging
+
+# local modules
+from satkit.commandLineInterface import Raw3D_CLI
+from satkit.io.ThreeD_ultrasound import ThreeD_Ultrasound
+from satkit import pd
+
+# how do we tell RawAndVideoCLI or RawCLI to run pd (and others) on all modalities. ie. how do we bind an operation to a modality?
+# answer: we hack it for now and document we've hacked it.
+
+
+def main():
+    # Run the command line interface.
+    function_dict = {'PD': (pd.addPD, [ThreeD_Ultrasound])}
+    Raw3D_CLI(
+        "PD processing script for raw ultrasound and videos", function_dict)
+
+
+if (__name__ == '__main__'):
+    t = time.time()
+    main()
+    elapsed_time = time.time() - t
+    logging.info('Elapsed time ' + str(elapsed_time))

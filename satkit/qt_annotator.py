1920f5f68bfb7fa1cea741aecb773aa4047a459f
Pertti Palo <pertti.palo@taurlin.org>
2021-11-15 02:29:09 +0100

    Qt based annotator which is able to open a window before crashing.

--- satkit/qt_annotator.py
+++ satkit/qt_annotator.py
@@ -34,13 +34,12 @@ from contextlib import closing
 from copy import deepcopy
 import csv
 import logging
-from pathlib import Path
 
 # Numpy
 import numpy as np
 
 # GUI functionality
-from PyQt5 import QFileDialog
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.uic import loadUiType
 
 # Plotting functions and hooks for GUI
@@ -59,7 +58,7 @@ Ui_MainWindow, QMainWindow = loadUiType('satkit/qt_annotator.ui')
 _qt_annotator_logger = logging.getLogger('satkit.qt_annotator')
 
 
-class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
+class PD_Qt_Annotator(QMainWindow, Ui_MainWindow):
     """
     Qt_Annotator_Window is a GUI class for annotating PD curves.
 
@@ -69,11 +68,39 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
     are provided, the acoustic segment boundaries.
     """
 
-    def __init__(self, recordings, args):
-        QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
+    default_categories = ['Stable', 'Hesitation', 'Chaos', 'No data',
+                          'Not set']
 
-        # from PD_annotator
+    @staticmethod
+    def line_xdirection_picker(line, mouseevent):
+        """
+        Find the nearest point in the x (time) direction from the mouseclick in
+        data coordinates. Return index of selected point, x and y coordinates of
+        the data at that point, and inaxes to enable originating subplot to be
+        identified.
+        """
+        if mouseevent.xdata is None:
+            return False, dict()
+        xdata = line.get_xdata()
+        ydata = line.get_ydata()
+        d = np.abs(xdata - mouseevent.xdata)
+
+        ind = np.argmin(d)
+        # if 1:
+        pickx = np.take(xdata, ind)
+        picky = np.take(ydata, ind)
+        props = dict(ind=ind,
+                     pickx=pickx,
+                     picky=picky,
+                     inaxes=mouseevent.inaxes)
+        return True, props
+        # else:
+        #     return False, dict()
+
+    def __init__(self, recordings, args, xlim=(-0.1, 1.0),
+                 categories=None):
+        super(PD_Qt_Annotator, self).__init__()
+
         self.setupUi(self)
 
         self.index = 0
@@ -82,93 +109,47 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
         self.recordings = recordings
         self.commandlineargs = args
 
-        self.fig = plt.figure(figsize=figsize)
+        if categories is None:
+            self.categories = PD_Qt_Annotator.default_categories
+        else:
+            self.categories = categories
+        self._addAnnotations()
+
+        self.fig_dict = {}
+
+        self.fig = Figure()
         self.keypress_id = self.fig.canvas.mpl_connect(
             'key_press_event', self.on_key)
 
         self.xlim = xlim
-        # PD_annotator ends
 
-        self.fig_dict = {}
-
-        self.mplfigs.itemClicked.connect(self.change_figure)
-
-        self.fig = Figure()
         #
         # Graphs to be annotated and the waveform for reference.
         #
         gs = self.fig.add_gridspec(4, 7)
         self.ax1 = self.fig.add_subplot(gs[0:0+3, 0:0+6])
         self.ax3 = self.fig.add_subplot(gs[3:3+1, 0:0+6])
-        # self.ax1 = fig.subplot2grid(
-        #     self.subplot_grid, (0, 0),
-        #     rowspan=3, colspan=6)
-        # self.ax3 = fig.subplot2grid(self.subplot_grid, (3, 0), colspan=6)
 
         self.draw_plots()
 
         self.fig.align_ylabels()
         self.fig.canvas.mpl_connect('pick_event', self.onpick)
 
-        self.add_mpl_elements(self.fig)
+        self.add_mpl_elements()
 
         self.show()
+        print(self.__dict__.keys())
 
-    def __init__(self, recordings, args, xlim=(-0.1, 1.0),
-                 figsize=(15, 6),
-                 categories=['Stable', 'Hesitation', 'Chaos', 'No data',
-                             'Not set']):
-        """ 
-        Constructor for the PD_Annotator GUI. 
+    @property
+    def current(self):
+        return self.recordings[self.index]
 
-        Also sets up internal state and adds keys [pdCategory, splineCategory, 
-        pdOnset, splineOnset] to the data argument. For the categories -1 is used
-        to mark 'not set', and for the onsets -1.0.
-        """
-        super().__init__(recordings, args, xlim, figsize)
-
-        self.categories = categories
-        self._addAnnotations()
-
-        #
-        # Subplot grid shape
-        #
-        subplot_grid = (4, 7)
-
-        #
-        # Graphs to be annotated and the waveform for reference.
-        #
-        self.ax1 = plt.subplot2grid(subplot_grid, (0, 0), rowspan=3, colspan=6)
-        self.ax3 = plt.subplot2grid(subplot_grid, (3, 0), colspan=6)
-
-        self.draw_plots()
-
-        self.fig.align_ylabels()
-        self.fig.canvas.mpl_connect('pick_event', self.onpick)
-
-        #
-        # Buttons and such.
-        #
-        self.ax4 = plt.subplot2grid(subplot_grid, (0, 6), rowspan=2)
-        self.ax4.axes.set_axis_off()
-        self.pdCategoryRB = RadioButtons(
-            self.ax4, self.categories,
-            active=self.current.annotations['pdCategory'])
-        self.pdCategoryRB.on_clicked(self.pdCatCB)
-
-        self.axnext = plt.axes([0.85, 0.225, 0.1, 0.055])
-        self.bnext = Button(self.axnext, 'Next')
-        self.bnext.on_clicked(self.next)
-
-        self.axprev = plt.axes([0.85, 0.15, 0.1, 0.055])
-        self.bprev = Button(self.axprev, 'Previous')
-        self.bprev.on_clicked(self.prev)
-
-        self.axsave = plt.axes([0.85, 0.05, 0.1, 0.055])
-        self.bsave = Button(self.axsave, 'Save')
-        self.bsave.on_clicked(self.save)
-
-        plt.show()
+    @property
+    def default_annotations(self):
+        return {
+            'pdCategory': len(self.categories)-1,
+            'pdOnset': -1.0,
+        }
 
     def _addAnnotations(self):
         for recording in self.recordings:
@@ -176,14 +157,6 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
                 recording.annotations.update(self.default_annotations)
             else:
                 recording.annotations = deepcopy(self.default_annotations)
-
-    def update(self):
-        """
-        Updates the graphs but not the buttons.
-        """
-        self.clear_axis()
-        self.draw_plots()
-        self.fig.canvas.draw()
 
     def _get_title(self):
         """ 
@@ -194,9 +167,72 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
         text += ', token: ' + str(self.index+1) + '/' + str(self.max_index)
         return text
 
-    @property
-    def current(self):
-        return self.recordings[self.index]
+    def clear_axis(self):
+        self.ax1.cla()
+        self.ax3.cla()
+
+    def update(self):
+        """
+        Updates the graphs but not the buttons.
+        """
+        self.clear_axis()
+        self.remove_mpl_elements()
+        self.draw_plots()
+        self.add_mpl_elements()
+        self.fig.canvas.draw()
+
+    def updateUI(self):
+        """
+        Updates parts of the UI outwith the graphs.
+        """
+        self.pdCategoryRB.set_active(self.current.annotations['pdCategory'])
+
+    def add_mpl_elements(self):
+        self.canvas = FigureCanvas(self.fig)
+        print(self.__dict__.keys())
+        self.mplWindowVerticalLayout.addWidget(self.canvas)
+        self.canvas.draw()
+        self.toolbar = NavigationToolbar(self.canvas,
+                                         self, coordinates=True)
+        self.addToolBar(self.toolbar)
+
+    def remove_mpl_elements(self):
+        self.mplWindowVerticalLayout.removeWidget(self.canvas)
+        self.canvas.close()
+        self.mplWindowVerticalLayout.removeWidget(self.toolbar)
+        self.toolbar.close()
+
+    def draw_plots(self):
+        """
+        Updates title and graphs. Called by self.update().
+        """
+        self.ax1.set_title(self._get_title())
+        self.ax1.axes.xaxis.set_ticklabels([])
+
+        audio = self.current.modalities['MonoAudio']
+        stimulus_onset = audio.meta['stimulus_onset']
+        wav = audio.data
+        wav_time = audio.timevector
+
+        pd = self.current.modalities['PD on ThreeD_Ultrasound']
+        ultra_time = pd.timevector - pd.timevector[-1] + wav_time[-1]
+
+        self.xlim = [ultra_time[0] - 0.05, ultra_time[-1]+0.05]
+
+        textgrid = self.current.textgrid
+
+        plot_pd_3d(
+            self.ax1, pd.data['pd'],
+            ultra_time, self.xlim, textgrid, stimulus_onset,
+            picker=PD_Qt_Annotator.line_xdirection_picker)
+        plot_wav_3D_ultra(self.ax3, wav, wav_time, self.xlim,
+                          textgrid, stimulus_onset)
+
+        if self.current.annotations['pdOnset'] > -1:
+            self.ax1.axvline(x=self.current.annotations['pdOnset'],
+                             linestyle=':', color="deepskyblue", lw=1)
+            self.ax3.axvline(x=self.current.annotations['pdOffset'],
+                             linestyle=':', color="deepskyblue", lw=1)
 
     def next(self, event):
         """
@@ -232,81 +268,6 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
             self.prev(event)
         elif event.key == "s":
             self.save(event)
-
-    @staticmethod
-    def line_xdirection_picker(line, mouseevent):
-        """
-        Find the nearest point in the x (time) direction from the mouseclick in
-        data coordinates. Return index of selected point, x and y coordinates of
-        the data at that point, and inaxes to enable originating subplot to be
-        identified.
-        """
-        if mouseevent.xdata is None:
-            return False, dict()
-        xdata = line.get_xdata()
-        ydata = line.get_ydata()
-        d = np.abs(xdata - mouseevent.xdata)
-
-        ind = np.argmin(d)
-        if 1:
-            pickx = np.take(xdata, ind)
-            picky = np.take(ydata, ind)
-            props = dict(ind=ind,
-                         pickx=pickx,
-                         picky=picky,
-                         inaxes=mouseevent.inaxes)
-            return True, props
-        else:
-            return False, dict()
-
-    @property
-    def default_annotations(self):
-        return {
-            'pdCategory': len(self.categories)-1,
-            'pdOnset': -1.0,
-        }
-
-    def draw_plots(self):
-        """
-        Updates title and graphs. Called by self.update().
-        """
-        self.ax1.set_title(self._get_title())
-        self.ax1.axes.xaxis.set_ticklabels([])
-
-        audio = self.current.modalities['MonoAudio']
-        stimulus_onset = audio.meta['stimulus_onset']
-        wav = audio.data
-        wav_time = audio.timevector
-
-        pd = self.current.modalities['PD on ThreeD_Ultrasound']
-        ultra_time = pd.timevector - pd.timevector[-1] + wav_time[-1]
-
-        self.xlim = [ultra_time[0] - 0.05, ultra_time[-1]+0.05]
-
-        textgrid = self.current.textgrid
-
-        plot_pd_3d(
-            self.ax1, pd.data['pd'],
-            ultra_time, self.xlim, textgrid, stimulus_onset,
-            picker=Qt_Annotator_Window.line_xdirection_picker)
-        plot_wav_3D_ultra(self.ax3, wav, wav_time, self.xlim,
-                          textgrid, stimulus_onset)
-
-        if self.current.annotations['pdOnset'] > -1:
-            self.ax1.axvline(x=self.current.annotations['pdOnset'],
-                             linestyle=':', color="deepskyblue", lw=1)
-            self.ax3.axvline(x=self.current.annotations['pdOffset'],
-                             linestyle=':', color="deepskyblue", lw=1)
-
-    def clear_axis(self):
-        self.ax1.cla()
-        self.ax3.cla()
-
-    def updateUI(self):
-        """ 
-        Updates parts of the UI outwith the graphs.
-        """
-        self.pdCategoryRB.set_active(self.current.annotations['pdCategory'])
 
     def save(self, event):
         """
@@ -356,26 +317,3 @@ class Qt_Annotator_Window(QMainWindow, Ui_MainWindow):
             self.current.annotations['pdOnset'] = event.pickx
 
         self.update()
-
-    def change_figure(self, item):
-        text = item.text()
-        self.rmmpl()
-        self.addmpl(self.fig_dict[text])
-
-    def add_figure(self, name, fig):
-        self.fig_dict[name] = fig
-        self.mplfigs.addItem(name)
-
-    def add_mpl_elements(self, fig):
-        self.canvas = FigureCanvas(fig)
-        self.mplWindowVerticalLayout.addWidget(self.canvas)
-        self.canvas.draw()
-        self.toolbar = NavigationToolbar(self.canvas,
-                                         self, coordinates=True)
-        self.addToolBar(self.toolbar)
-
-    def remove_mpl_elements(self):
-        self.mplWindowVerticalLayout.removeWidget(self.canvas)
-        self.canvas.close()
-        self.mplWindowVerticalLayout.removeWidget(self.toolbar)
-        self.toolbar.close()

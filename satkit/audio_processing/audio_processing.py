bb48e8dbe4bac3fe02de450d14c36d0f0ec1af9b
Pertti Palo <giuthas@gmail.com>
2022-06-20 23:05:03 +0300

    Docstrings and style updates.

--- satkit/audio_processing/audio_processing.py
+++ satkit/audio_processing/audio_processing.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2019-2022 Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
 #
-# This file is part of Speech Articulation ToolKIT 
+# This file is part of Speech Articulation ToolKIT
 # (see https://github.com/giuthas/satkit/).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -40,61 +40,82 @@ from scipy.signal import butter, filtfilt, kaiser, sosfilt
 _audio_logger = logging.getLogger('satkit.audio')
 
 
-def high_pass_50(fs):
+def high_pass_50(sampling_frequency):
     """Returns a high-pass filter with a 50Hz stop band. Used for
     filtering the mains frequency away from recorded sound."""
     _audio_logger.debug("Generating high-pass filter.")
-    stop = (50/(fs/2))  # 50 Hz stop band
+    stop = (50/(sampling_frequency/2))  # 50 Hz stop band
     b, a = butter(10, stop, 'highpass')
     return(b, a)
 
 
-def high_pass(fs, sb):
+def high_pass(sampling_frequency, stop_band):
     """Returns a high-pass filter with a stop band of sb. Used for
     filtering the mains frequency away from recorded sound."""
     _audio_logger.debug("Generating high-pass filter.")
-    stop = (sb/(fs/2))
+    stop = (stop_band/(sampling_frequency/2))
     b, a = butter(10, stop, 'highpass')
     return {'b': b, 'a': a}
 
 
-def band_pass(fs):
+def band_pass(sampling_frequency):
+    """Generate a band pass filter for detecting a 1kHz signal."""
     _audio_logger.debug("Generating band-pass filter.")
-    nyq = 0.5 * fs
+    nyq = 0.5 * sampling_frequency
     low = 950.0 / nyq
     high = 1050.0 / nyq
     sos = butter(1, [low, high], btype='band', output='sos')
     return(sos)
 
 
-def detect_beep_and_speech(frames, fs, b, a, name):
+def detect_beep_and_speech(frames, sampling_frequency, b, a, name):
+    """
+    Find a 1kHz 50ms beep at the beginning of a sound sample.
+
+    This functions is for processing delayed naming data
+    where the go-signal is a 1kHz 50ms beep. The algorithm assumes
+    that the signal is the first properly detectable sound in
+    the sample and also that it starts with a rising edge.
+    The detection is based on locating the first negative
+    valued half wave (the second half wave) and working backwards
+    from that using zero crossings and wave duration to pinpoint
+    the onset.
+
+    Parameters:
+    frames: the sound sample
+    sampling_frequency: the sampling frequency of the sound sample
+    b and a: high pass filter parameters to remove the electrical
+        mains' interference
+    name: name identifying the sample. Usually the filename.
+1    """
 
     _audio_logger.debug(
-        "Detecting beep onset and presence of speech in " + name + ".")
+        "Detecting beep onset and presence of speech in {name}.",
+        name = name)
     hp_signal = filtfilt(b, a, frames)
-    sos = band_pass(fs)
+    sos = band_pass(sampling_frequency)
     bp_signal = sosfilt(sos, frames)
     bp_signal = sosfilt(sos, bp_signal[::-1])[::-1]
-    n = len(hp_signal)
+    signal_length = len(hp_signal)
 
     # 1 ms window
-    window_length = int(0.001*fs)
+    window_length = int(0.001*sampling_frequency)
     half_window_length = int(window_length/2)
 
     # pad with zeros at both ends
-    padded_signal = np.zeros(n + window_length)
-    padded_signal[half_window_length: half_window_length+n] = \
+    padded_signal = np.zeros(signal_length + window_length)
+    padded_signal[half_window_length: half_window_length+signal_length] = \
         np.square(hp_signal)  # squared already for rms, r&m later
-    padded_signal2 = np.zeros(n + window_length)
-    padded_signal2[half_window_length: half_window_length+n] = \
+    padded_signal2 = np.zeros(signal_length + window_length)
+    padded_signal2[half_window_length: half_window_length+signal_length] = \
         np.square(bp_signal)  # squared already for rms, r&m later
 
     # square windowed samples
-    wind_signal = np.zeros((n, window_length))
-    bp_wind_signal = np.zeros((n, window_length))
+    wind_signal = np.zeros((signal_length, window_length))
+    bp_wind_signal = np.zeros((signal_length, window_length))
     for i in range(window_length):
-        wind_signal[:, i] = padded_signal[i:i+n]
-        bp_wind_signal[:, i] = padded_signal2[i:i+n]
+        wind_signal[:, i] = padded_signal[i:i+signal_length]
+        bp_wind_signal[:, i] = padded_signal2[i:i+signal_length]
 
     # kaiser windowed samples
     intensity_window = kaiser(window_length, 20)  # copied from praat
@@ -109,7 +130,7 @@ def detect_beep_and_speech(frames, fs, b, a, name):
     # Old int_time was used to almost correct the shift caused by windowing.
     # int_time = np.linspace(0, float(len(hp_signal) +
     # (window_length%2 - 1)/2.0)/fs, len(int_signal))
-    int_time = np.linspace(0, float(len(hp_signal))/fs, len(hp_signal))
+    int_time = np.linspace(0, float(len(hp_signal))/sampling_frequency, len(hp_signal))
     int_signal[int_time < 1] = -80
 
     # First form a rough estimate of where the beep is by detecting the first
@@ -120,8 +141,8 @@ def detect_beep_and_speech(frames, fs, b, a, name):
 
     # Search for the actual beep in the area from beginning of the recording to
     # 25 ms before and after where band passing thinks the beep begins.
-    roi_beg = bp_spike_indeces[0][0] - int(0.025*fs)
-    roi_end = bp_spike_indeces[0][0] + int(0.025*fs)
+    roi_beg = bp_spike_indeces[0][0] - int(0.025*sampling_frequency)
+    roi_end = bp_spike_indeces[0][0] + int(0.025*sampling_frequency)
 
     # Find the first properly rising edge in the 50 ms window.
     threshold = .1*min(frames[0:roi_end])
@@ -131,12 +152,12 @@ def detect_beep_and_speech(frames, fs, b, a, name):
 
     zero_crossings = np.where(
         np.diff(np.signbit(frames[beep_approx_index:roi_end])))[0]
-    beep_index = beep_approx_index + zero_crossings[0] + 1 - int(.001*fs)
+    beep_index = beep_approx_index + zero_crossings[0] + 1 - int(.001*sampling_frequency)
     beep = int_time[beep_index]
 
     # check if the energy before the beep begins is less
     # than the energy after the beep.
-    split_point = beep_index + int(.075*fs)
+    split_point = beep_index + int(.075*sampling_frequency)
     if len(hp_signal) > split_point:
         ave_energy_pre_beep = np.sum(int_signal[:beep_index])/beep_index
         ave_energy_post_beep = np.sum(

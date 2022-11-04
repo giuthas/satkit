3819e4d250aac0e5cdb98de5f8c09b81624de808
Pertti Palo <giuthas@gmail.com>
2022-11-04 00:39:31 +0000

    Working version on moving an annotation line on all axes.

--- satkit/gui/annotation_boundary.py
+++ satkit/gui/annotation_boundary.py
@@ -143,63 +143,86 @@ class AnnotationBoundary:
     lock = None  # only one can be animated at a time
 
     def __init__(self, lines, annotation=None):
-        self.line = lines[0]
+        self.lines = lines
         self.annotation = annotation
         self.press = None
-        self.background = None
+        self.backgrounds = []
 
     def connect(self):
         """Connect to all the events we need."""
-        self.cidpress = self.line.figure.canvas.mpl_connect(
-            'button_press_event', self.on_press)
-        self.cidrelease = self.line.figure.canvas.mpl_connect(
-            'button_release_event', self.on_release)
-        self.cidmotion = self.line.figure.canvas.mpl_connect(
-            'motion_notify_event', self.on_motion)
+        for line in self.lines:
+            self.cidpress = line.figure.canvas.mpl_connect(
+                'button_press_event', self.on_press)
+            self.cidrelease = line.figure.canvas.mpl_connect(
+                'button_release_event', self.on_release)
+            self.cidmotion = line.figure.canvas.mpl_connect(
+                'motion_notify_event', self.on_motion)
 
     def on_press(self, event):
         """Check whether mouse is over us; if so, store some data."""
-        if (event.inaxes != self.line.axes
-                or AnnotationBoundary.lock is not None):
+        if AnnotationBoundary.lock is not None:
             return
-        contains, attrd = self.line.contains(event)
-        if not contains:
+
+        is_inaxes = False
+        for line in self.lines:
+            if (event.inaxes == line.axes):
+                is_inaxes = True
+        if not is_inaxes:
             return
-        self.press = self.line.get_data(), (event.xdata, event.ydata)
+
+        some_line_contains = False
+        for line in self.lines:
+            contains, attrd = line.contains(event)
+            if contains:
+                some_line_contains = True
+                break
+        if not some_line_contains:
+            return
+
+        self.press = line.get_data(), (event.xdata, event.ydata)
         AnnotationBoundary.lock = self
 
         # draw everything but the selected line and store the pixel buffer
-        canvas = self.line.figure.canvas
-        axes = self.line.axes
-        self.line.set_animated(True)
-        canvas.draw()
-        self.background = canvas.copy_from_bbox(self.line.axes.bbox)
+        for line in self.lines:
+            canvas = line.figure.canvas
+            axes = line.axes
+            line.set_animated(True)
+            canvas.draw()
+            self.backgrounds.append(canvas.copy_from_bbox(line.axes.bbox))
 
-        # now redraw just the line
-        axes.draw_artist(self.line)
+            # now redraw just the line
+            axes.draw_artist(line)
 
-        # and blit just the redrawn area
-        canvas.blit(axes.bbox)
+            # and blit just the redrawn area
+            canvas.blit(axes.bbox)
 
     def on_motion(self, event):
         """Move the rectangle if the mouse is over us."""
-        if (event.inaxes != self.line.axes
-                or AnnotationBoundary.lock is not self):
+        if AnnotationBoundary.lock is not self:
             return
+
+        is_inaxes = False
+        for line in self.lines:
+            if (event.inaxes == line.axes):
+                is_inaxes = True
+        if not is_inaxes:
+            return
+
         (x0, y0), (xpress, ypress) = self.press
         dx = event.xdata - xpress
-        self.line.set(xdata=x0+dx)
+        for i, line in enumerate(self.lines):
+            line.set(xdata=x0+dx)
 
-        canvas = self.line.figure.canvas
-        axes = self.line.axes
-        # restore the background region
-        canvas.restore_region(self.background)
+            canvas = line.figure.canvas
+            axes = line.axes
+            # restore the background region
+            canvas.restore_region(self.backgrounds[i])
 
-        # redraw just the current rectangle
-        axes.draw_artist(self.line)
+            # redraw just the current rectangle
+            axes.draw_artist(line)
 
-        # blit just the redrawn area
-        canvas.blit(axes.bbox)
+            # blit just the redrawn area
+            canvas.blit(axes.bbox)
 
     def on_release(self, event):
         """Clear button press information."""
@@ -210,17 +233,19 @@ class AnnotationBoundary:
         AnnotationBoundary.lock = None
 
         # turn off the rect animation property and reset the background
-        self.line.set_animated(False)
-        self.background = None
+        for line in self.lines:
+            line.set_animated(False)
+            # redraw the full figure
+            line.figure.canvas.draw()
 
-        # redraw the full figure
-        self.line.figure.canvas.draw()
+        self.backgrounds = []
 
     def disconnect(self):
         """Disconnect all callbacks."""
-        self.line.figure.canvas.mpl_disconnect(self.cidpress)
-        self.line.figure.canvas.mpl_disconnect(self.cidrelease)
-        self.line.figure.canvas.mpl_disconnect(self.cidmotion)
+        for line in self.lines:
+            line.figure.canvas.mpl_disconnect(self.cidpress)
+            line.figure.canvas.mpl_disconnect(self.cidrelease)
+            line.figure.canvas.mpl_disconnect(self.cidmotion)
 
 # fig, ax = plt.subplots()
 # lines = []

032288d32cbbc93ccf880b620eabbc45f1968c30
Pertti Palo <giuthas@gmail.com>
2022-11-28 18:10:14 -0500

    Moved gui config parameters to there own file.

--- satkit/configuration.py
+++ satkit/configuration.py
@@ -10,6 +10,7 @@ from satkit.data_import.datasource import Datasource
 
 config = {}
 data_run_params = {}
+gui_params = {}
 
 # This is where we store the metadata needed to write out the configuration and
 # possibly not mess up the comments in it.
@@ -73,7 +74,8 @@ def load_config(filepath: Union[Path, str, None]=None) -> None:
                     "data": Int(), 
                     "tier": Int()
                     }),
-                "data run parameter file": PathValidator()
+                "data run parameter file": PathValidator(),
+                "gui parameter file": PathValidator()
                 })
             try:
                 _raw_config_dict = load(yaml_file.read(), schema)
@@ -121,10 +123,6 @@ def load_run_params(filepath: Union[Path, str, None]=None) -> None:
                     "test": Bool(),
                     "file": Bool(),
                     "utterance": Bool()
-                    }),
-                "gui params": Map({
-                    "data axes": Seq(Str()),
-                    "pervasive tiers": Seq(Str())
                     })
                 })
             try:
@@ -137,3 +135,38 @@ def load_run_params(filepath: Union[Path, str, None]=None) -> None:
         print(f"Didn't find {filepath}. Exiting.".format(str(filepath)))
         sys.exit()
     data_run_params.update(_raw_data_run_params_dict.data)
+
+def load_gui_params(filepath: Union[Path, str, None]=None) -> None:
+    """
+    Read the config file from filepath.
+    
+    If filepath is None, read from the default file
+    'configuration/configuration.yaml'. In both cases if the file does not
+    exist, report this and exit.
+    """
+    if filepath is None:
+        print(f"Fatal error in reading {filepath}:")
+        print(error)
+        sys.exit()
+    elif isinstance(filepath, str):
+        filepath = Path(filepath)
+
+    global gui_run_params
+    global _raw_gui_run_params_dict
+
+    if filepath.is_file():
+        with closing(open(filepath, 'r')) as yaml_file:
+            schema = Map({
+                "data axes": Seq(Str()),
+                "pervasive tiers": Seq(Str())
+                })
+            try:
+                _raw_gui_params_dict = load(yaml_file.read(), schema)
+            except YAMLError as error:
+                print(f"Fatal error in reading {filepath}:")
+                print(error)
+                sys.exit()
+    else:
+        print(f"Didn't find {filepath}. Exiting.".format(str(filepath)))
+        sys.exit()
+    gui_params.update(_raw_gui_params_dict.data)

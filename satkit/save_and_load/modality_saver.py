42ec02d32c2c66e0c2c22906bd39435f4f077d31
Pertti Palo <giuthas@gmail.com>
2022-12-31 07:56:18 -0500

    Part of saving works.

--- satkit/save_and_load/modality_saver.py
+++ satkit/save_and_load/modality_saver.py
@@ -1,19 +1,25 @@
 import logging
 from typing import List
 
+import nestedtext
 from satkit.data_structures import Modality, Recording
 
 _modality_saver_logger = logging.getLogger('satkit.modality_saver')
 
-def save_modality_data(modality: Modality):
+def save_modality_data(modality: Modality) -> str:
     """
     Save the data of a Modality.
 
     This saves only ModalityData.data and ModalityData.timevector.
     """
     _modality_saver_logger.debug("Saving data for %s."%modality.name)
+    suffix = modality.name.replace(" ", "_")
+    filename = f"{modality.recording.basename}.{suffix}"
+    _modality_saver_logger.debug("Wrote file %s."%(filename))
 
-def save_modality_meta(modality: Modality):
+    return filename
+
+def save_modality_meta(modality: Modality) -> str:
     """
     Save meta data and annotations for a Modality.
 
@@ -21,25 +27,53 @@ def save_modality_meta(modality: Modality):
     needed to reconstruct the Modality. 
     """
     _modality_saver_logger.debug("Saving meta for %s."%modality.name)
+    suffix = modality.name.replace(" ", "_")
+    filename = f"{modality.recording.basename}.{suffix}"
+    filename += "meta"
+    _modality_saver_logger.debug("Wrote file %s."%(filename))
+
+    return filename
     
 
-def save_recording_meta(recording: Recording, meta: dict):
+def save_recording_meta(recording: Recording, meta: dict) -> str:
     """
     Save Recording meta.
 
     The meta dict should contain at least a list of the modalities this recording
     has and their saving locations.
     """
-    _modality_saver_logger.debug("Saving meta for recording %s."%recording.name)
+    _modality_saver_logger.debug("Saving meta for recording %s."%recording.basename)
+    filename = f"{recording.basename}.satkit_meta"
+    filepath = recording.path/filename
+    try:
+        nestedtext.dump(meta, filepath)
+    except nestedtext.NestedTextError as error:
+        error.terminate()
+    _modality_saver_logger.debug("Wrote file %s."%(filename))
+
+    return filename
 
 def save_derived_modalities(recording: Recording):
     """
     Save derived data modalities for a single Recording.
     """
-    for modality in recording.modalities:
-        if recording.modalities[modality].is_derived_modality:
-            save_modality_data(recording.modalities[modality])
-            save_modality_meta(recording.modalities[modality])
+    recording_meta = {}
+    for modality_name in recording.modalities:
+        modality_meta = {}
+        modality = modality
+        if modality.is_derived_modality:
+            modality_meta['data'] = save_modality_data(modality)
+            modality_meta['meta'] = save_modality_meta(modality)
+        else:
+            modality_meta['data'] = str(modality.data_path)
+            modality_meta['meta'] = {
+                'sampling_rate': modality.sampling_rate
+                }
+            if hasattr(modality, 'meta'):
+                modality_meta['meta'].update(modality.meta)
+                modality_meta['meta']['meta_file'] = str(modality_meta['meta']['meta_file']) 
+        recording_meta[modality_name] = modality_meta
+    save_recording_meta(recording, recording_meta)
 
 def save_derived_data(recordings: List[Recording], save_excluded: bool=True):
     """

b54b4385b02654d008a0ef1a62d3df2934ae47bd
Pertti Palo <giuthas@gmail.com>
2023-05-11 00:17:02 -0500

    First prototype of peak detection comparisons.

--- satkit.py
+++ satkit.py
@@ -83,6 +83,8 @@ def main():
     }
     process_data(recordings=recordings, processing_functions=function_dict)
 
+    peaks.save_peaks('pd_l1', recordings)
+
     # operation = Operation(
     #     processing_function = pd.add_pd, 
     #     modality = RawUltrasound, 

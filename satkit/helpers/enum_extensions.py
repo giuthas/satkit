6e13d7041f26ce3c86a1e4b6ded8ada30c66bdcd
Pertti Palo <giuthas@gmail.com>
2023-11-14 15:26:33 -0500

    Added license info.

--- satkit/helpers/enum_extensions.py
+++ satkit/helpers/enum_extensions.py
@@ -1,7 +1,38 @@
+#
+# Copyright (c) 2019-2023
+# Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
+#
+# This file is part of Speech Articulation ToolKIT
+# (see https://github.com/giuthas/satkit/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# The example data packaged with this program is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License. You should have received a
+# copy of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License along with the data. If not,
+# see <https://creativecommons.org/licenses/by-nc-sa/4.0/> for details.
+#
+# When using the toolkit for scientific publications, please cite the
+# articles listed in README.markdown. They can also be found in
+# citations.bib in BibTeX format.
+#
 """
-Enum union based on and compatible with the standard library's `enum`.
-"""
+Enum extensions that are compatible with the standard library's `enum`. 
 
+Original UnionEnum implementation licensed with
 # MIT License
 #
 # Copyright (c) 2020 Paolo Lammens
@@ -23,6 +54,9 @@ Enum union based on and compatible with the standard library's `enum`.
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+All other parts licensed with the usual SATKIT license.
+"""
 
 from enum import Enum, EnumMeta
 import itertools as itt

85cbfed93df98f83be046c3f0c2393fa78a597fe
Pertti Palo <giuthas@gmail.com>
2023-11-20 12:56:51 -0500

    Another todo

--- satkit/qt_annotator.py
+++ satkit/qt_annotator.py
@@ -657,6 +657,9 @@ class PdQtAnnotator(QMainWindow, Ui_MainWindow):
                 # ic(epsilon, splines.timevector[spline_index] - timestamp)
                 # ic(splines.timevector[spline_index], timestamp)
                 if (splines.timevector[spline_index] - timestamp) < epsilon:
+                    # TODO: pass SplineMetricParameters or where ever
+                    # exclude_points ends up being stored to plot_spline so that it
+                    # can display only the analysed part.
                     plot_spline(self.ultra_axes,
                                 splines.cartesian_spline(spline_index)[:, 11:])
                 else:

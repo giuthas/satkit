f226f5d47d1a158b147cb650f494eabdd2d6546c
Pertti Palo <giuthas@gmail.com>
2023-11-27 02:33:15 -0500

    New module for classes used in configuration.

---
+++ satkit/configuration/configuration_classes.py
@@ -0,0 +1,50 @@
+#
+# Copyright (c) 2019-2023
+# Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
+#
+# This file is part of Speech Articulation ToolKIT
+# (see https://github.com/giuthas/satkit/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# The example data packaged with this program is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License. You should have received a
+# copy of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License along with the data. If not,
+# see <https://creativecommons.org/licenses/by-nc-sa/4.0/> for details.
+#
+# When using the toolkit for scientific publications, please cite the
+# articles listed in README.markdown. They can also be found in
+# citations.bib in BibTeX format.
+#
+"""
+Classes for dealing with configuration.
+
+This file exist to remove a namespace conflict between typing.Optional and
+strictyaml.Optional.
+"""
+
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class ExclusionList:
+    """
+    List of files, prompts, and parts of prompts to be excluded from analysis.
+    """
+    files: Optional[list[str]] = None
+    prompts: Optional[list[str]] = None
+    parts_of_prompts: Optional[list[str]] = None

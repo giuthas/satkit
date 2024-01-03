c30fbb4d17f4717b63791f2b02277bfeb2d2af39
Pertti Palo <giuthas@gmail.com>
2024-01-03 16:09:25 -0700

    Changes to keep the ide happy. This file is to be removed before 1.0

--- satkit/gui/annotator.py
+++ satkit/gui/annotator.py
@@ -51,7 +51,7 @@ from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.uic import loadUiType
-from satkit.configuration.configuration import config_dict, data_run_params
+from satkit.configuration.config import config_dict, data_run_params
 from satkit.gui.qt_annotator_window import QtAnnotatorWindow
 from satkit.plot_and_publish import plot_timeseries, plot_wav
 from satkit.plot_and_publish.plot import plot_satgrid_tier

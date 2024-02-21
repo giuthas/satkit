d407dc4f77dfad2d5c96cfba045d9a77a585b060
Pertti Palo <giuthas@gmail.com>
2024-02-21 15:30:38 -0500

    New file for code/IDE friendly configuration models.

---
+++ satkit/configuration/configuration_models.py
@@ -0,0 +1,46 @@
+#
+# Copyright (c) 2019-2024
+# Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
+#
+# This file is part of Speech Articulation ToolKIT
+# (see https://github.com/giuthas/satkit/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# The example data packaged with this program is licensed under the
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License. You should have received a
+# copy of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+# International (CC BY-NC-SA 4.0) License along with the data. If not,
+# see <https://creativecommons.org/licenses/by-nc-sa/4.0/> for details.
+#
+# When using the toolkit for scientific publications, please cite the
+# articles listed in README.markdown. They can also be found in
+# citations.bib in BibTeX format.
+#
+"""
+These are the code friendly wrappers for the configuration structures.
+
+configuration_parsers contains the actual parsing of strictyaml into
+comment-retaining dictionary-like structures. Here those structures get parsed
+into pydantic models that know what their fields actually are.
+
+This two level structure is maintained so that in some version after SATKIT 1.0
+we can implement configuration round tripping with preserved comments.
+"""
+import logging
+
+from satkit.helpers.base_model_extensions import UpdatableBaseModel
+
+_logger = logging.getLogger('satkit.configuration_models')

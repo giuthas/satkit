688ad56a59523f883292ff7caca48ae0f5241285
Pertti Palo <giuthas@gmail.com>
2024-10-01 20:58:29 -0600

    Added FileInformation to the public interface.

--- satkit/data_structures/__init__.py
+++ satkit/data_structures/__init__.py
@@ -39,6 +39,7 @@ SATKIT's datastructures and are not meant to be directly inherited from.
 from .base_classes import Statistic
 from .data_structures import Modality, Recording, Session
 from .meta_data_classes import (
+    FileInformation,
     ModalityData, ModalityMetaData, PointAnnotations,
     RecordingMetaData, SessionConfig, StatisticMetaData
 )

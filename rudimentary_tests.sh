36bbcfd52b43a36b4016bc76b9be1b848390b7c1
Pertti Palo <giuthas@gmail.com>
2024-10-03 17:34:35 -0600

    Added the next scenario.

--- rudimentary_tests.sh
+++ rudimentary_tests.sh
@@ -1,2 +1,6 @@
 # This should return with instructions for how to run SATKIT
 ./satkit.py
+
+# This should run with the default configuration and show 10 recordings in the
+# GUI.
+./satkit.py tongue_data_1_1/

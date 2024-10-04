907836d2cd392d2ec2a48d0839bd0c646eb8b563
Pertti Palo <giuthas@gmail.com>
2024-10-03 18:03:30 -0600

    Two more scenarios.

--- rudimentary_tests.sh
+++ rudimentary_tests.sh
@@ -1,6 +1,13 @@
-# This should return with instructions for how to run SATKIT
+# Return instructions for how to run SATKIT
 ./satkit.py
 
-# This should run with the default configuration and show 10 recordings in the
-# GUI.
+# Run with the default configuration and show 10 recordings in the
+# GUI. 
+#   - Just files, nothing fancy.
 ./satkit.py tongue_data_1_1/
+
+#   - Missing files
+./satkit.py tongue_data_1_2/
+
+#   - Missing files, exclusion list
+./satkit.py tongue_data_1_2/ -e tongue_data_1_2/exclusion_list.csv

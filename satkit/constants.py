59a26879bbd2ced080cbe968e54bb8d7fe016d79
Pertti Palo <giuthas@gmail.com>
2024-12-24 23:38:29 -0500

    Bumped version number.

--- satkit/constants.py
+++ satkit/constants.py
@@ -43,7 +43,7 @@ from dataclasses import dataclass
 from enum import Enum
 
 # TODO 1.0: Decouple program and file format versions at version 1.0.
-SATKIT_VERSION = '0.11'
+SATKIT_VERSION = '0.12'
 SATKIT_FILE_VERSION = SATKIT_VERSION
 
 DEFAULT_ENCODING = 'utf-8'

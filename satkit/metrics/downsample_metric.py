d8166f25b1cbc0f3f1018de5605ec2579ec67ba5
Pertti Palo <giuthas@gmail.com>
2025-01-07 13:11:53 -0500

    Updated license headers.

--- satkit/metrics/downsample_metric.py
+++ satkit/metrics/downsample_metric.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019-2024
+# Copyright (c) 2019-2025
 # Pertti Palo, Scott Moisik, Matthew Faytak, and Motoki Saito.
 #
 # This file is part of Speech Articulation ToolKIT
